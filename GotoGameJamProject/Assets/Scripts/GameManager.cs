2121b3fba63c9feac19927099d87cf13710e53d4
CNicoF <nicolasferreira-externo@tgacompany.com>
2021-10-31 20:38:24 -0300

    Game manager ahora es Singleton

--- GotoGameJamProject/Assets/Scripts/GameManager.cs
+++ GotoGameJamProject/Assets/Scripts/GameManager.cs
@@ -1,8 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    //Hola
+    public static GameManager instance;
+
+    //ser unico y existir en todo momento
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    //manejar el cambio de escenas
+    public void NextScene()
+    {
+        //cambia a la proxima escena segun el orden del build.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1 );
+    }
+
+
 }
