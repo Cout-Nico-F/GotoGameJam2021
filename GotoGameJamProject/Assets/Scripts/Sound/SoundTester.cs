04dc819dad14563f1eca61be5b64b00ec3eebfd5
CNicoF <nicolasferreira-externo@tgacompany.com>
2021-10-31 16:43:06 -0300

    Probando AudioManager estilo brackeys

---
+++ GotoGameJamProject/Assets/Scripts/Sound/SoundTester.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AudioJam
+{
+
+    public class SoundTester : MonoBehaviour
+    {
+        public void Awake()
+        {
+            SoundManager.instance.Play("TestFiesta");
+            Debug.Log("wtf");
+        }
+    }
+
+}
