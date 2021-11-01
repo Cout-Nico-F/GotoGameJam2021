e259d35706292968ff17e845580b670c69cddb6f
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2021-11-01 20:23:07 -0300

    Prueba platafomas 2D
    Añadido scripts,sprites y necesarios para realizar movimiento basico de plataformas 2D

---
+++ GotoGameJamProject/Assets/Plataformer Movement TEST/Scripts/PlatformerMovement2D.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerMovement2D : MonoBehaviour
+{
+    /*****Movimiento*****/
+    [Header("Movement")]
+    [SerializeField]
+    float Speed = 0;
+    [SerializeField]
+    float RaycastDistanceLeft_Right;
+    [SerializeField]
+    Vector3 OffsetRaycastLeft_Right;
+    Rigidbody2D rb2d;
+
+    /******Salto********/
+    [Header("Jump")]
+    [SerializeField]
+    float JumpForce;
+    [SerializeField]
+    float Gravity;
+    [SerializeField]
+    Vector3 OffsetRaycast;
+    [SerializeField]
+    float RaycastDistanceJump;
+    bool inFloor;
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        inFloor = VerifyFloor();
+        HorizontalMovement();
+        Jump();
+    }
+
+
+
+    private bool VerifyFloor()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position-OffsetRaycast, Vector2.down, RaycastDistanceJump);
+        if (hit.collider != null)
+        {
+            if (hit.collider.CompareTag("Floor"))
+            { Debug.Log("Suelo"); return true; }
+        }
+        return false;
+    }
+
+    private void HorizontalMovement()
+    {
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        Vector2 LateralMovement = new Vector2(Speed*HorizontalInput,0);
+        LateralMovement *= Time.deltaTime;
+        transform.Translate(LateralMovement);
+    }
+
+    private void Jump()
+    {
+        /**Gravedad**/
+        if(!inFloor)
+        { rb2d.AddForce(Vector2.down * Time.deltaTime * Gravity); }
+        /**Salto**/
+        if(Input.GetButton("Jump")&&inFloor)
+        {rb2d.AddForce(Vector2.up*Time.deltaTime*JumpForce);}
+    }
+}
