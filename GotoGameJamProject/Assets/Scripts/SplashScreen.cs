2dd994bee2f8667b45730314555da23a2dbfaf96
CNicoF <nicolasferreira-externo@tgacompany.com>
2021-10-31 21:35:40 -0300

    Metodo viejo vacio

--- GotoGameJamProject/Assets/Scripts/SplashScreen.cs
+++ GotoGameJamProject/Assets/Scripts/SplashScreen.cs
@@ -17,13 +17,7 @@ public class SplashScreen : MonoBehaviour
         yield return new WaitForSeconds(waitingTime);
         NextScene();
     }
-
-    //contar X segundos atras
-    private void CountDownSecs(int secs)
-    {
-
-    }
-   
+  
     //decirle al game manager que pase de escena.
     private void NextScene()
     {
