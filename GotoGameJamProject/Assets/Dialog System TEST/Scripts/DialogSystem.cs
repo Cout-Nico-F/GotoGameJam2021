44ea1a05c2b27baf8302d2527fdbcba019e5df08
CNicoF <nicolasferreira-externo@tgacompany.com>
2021-11-02 09:22:07 -0300

    Nomenclatura: Bajadas las mayusculas de las variables

--- GotoGameJamProject/Assets/Dialog System TEST/Scripts/DialogSystem.cs
+++ GotoGameJamProject/Assets/Dialog System TEST/Scripts/DialogSystem.cs
@@ -8,29 +8,30 @@ namespace AudioJam
     public class DialogSystem : MonoBehaviour
     {
         [Header("Personalize Text")]
-        [SerializeField] private string Input;
-        [SerializeField] private Color TextColor;
-        [SerializeField] private Font TextFont;
-        [SerializeField] private float Delay;
-        [SerializeField] private int TextSize;
-        [SerializeField] private string NameFXsound;
+        [SerializeField] private string input;
+        [SerializeField] private Color textColor;
+        [SerializeField] private Font textFont;
+        [SerializeField] private float delay;
+        [SerializeField] private int textSize;
+        [SerializeField] private string nameFXsound;
         private Text TextHolder;
 
         public void Start()
         {
             TextHolder = GetComponent<Text>();
-            StartCoroutine(WriteText(Input, TextHolder, TextColor, TextFont, Delay, TextSize,NameFXsound));
+            StartCoroutine(WriteText(input, TextHolder, textColor, textFont, delay, textSize,nameFXsound));
         }
-        public IEnumerator WriteText(string input, Text TextHolder, Color TextColor, Font TextFont, float Delay, int TextSize,string NameFXsound)
+
+        public IEnumerator WriteText(string input, Text textHolder, Color textColor, Font textFont, float delay, int textSize,string nameFXsound)
         {
-            TextHolder.font = TextFont;
-            TextHolder.color = TextColor;
-            TextHolder.fontSize = TextSize;
+            textHolder.font = textFont;
+            textHolder.color = textColor;
+            textHolder.fontSize = textSize;
             for (int i = 0; i < input.Length; i++)
             {
-                TextHolder.text += input[i];
-                SoundManager.instance.Play(NameFXsound);
-                yield return new WaitForSeconds(Delay);
+                textHolder.text += input[i];
+                SoundManager.instance.Play(nameFXsound);
+                yield return new WaitForSeconds(delay);
             }
         }
     }
