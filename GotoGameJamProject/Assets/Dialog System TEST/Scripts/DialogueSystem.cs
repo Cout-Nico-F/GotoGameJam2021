3c9d607b1fc99be81788c008d5d5ce8f646a5b34
CNicoF <nicolasferreira-externo@tgacompany.com>
2021-11-02 09:25:15 -0300

    DialogueSystem rename + no pertenece al namespace audio, pero lo usa (using)

---
+++ GotoGameJamProject/Assets/Dialog System TEST/Scripts/DialogueSystem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using AudioJam;
+
+public class DialogueSystem : MonoBehaviour
+{
+    [Header("Personalize Text")]
+    [SerializeField] private string input;
+    [SerializeField] private Color textColor;
+    [SerializeField] private Font textFont;
+    [SerializeField] private float delay;
+    [SerializeField] private int textSize;
+    [SerializeField] private string nameFXsound;
+    private Text TextHolder;
+
+    public void Start()
+    {
+        TextHolder = GetComponent<Text>();
+        StartCoroutine(WriteText(input, TextHolder, textColor, textFont, delay, textSize, nameFXsound));
+    }
+
+    public IEnumerator WriteText(string input, Text textHolder, Color textColor, Font textFont, float delay, int textSize, string nameFXsound)
+    {
+        textHolder.font = textFont;
+        textHolder.color = textColor;
+        textHolder.fontSize = textSize;
+        for (int i = 0; i < input.Length; i++)
+        {
+            textHolder.text += input[i];
+            SoundManager.instance.Play(nameFXsound);
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
