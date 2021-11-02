c73987078d6d0dd0e29cac1a69ef6650f887efc1
CNicoF <nicolasferreira-externo@tgacompany.com>
2021-11-02 11:09:54 -0300

    Modifique el dialoguesystem para usar TMPro y ser mas responsive a futuro
    Vamos a poder usar desktop + mobile + webgl para el build

--- GotoGameJamProject/Assets/Dialog System TEST/Scripts/DialogueSystem.cs
+++ GotoGameJamProject/Assets/Dialog System TEST/Scripts/DialogueSystem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 using AudioJam;
 
 public class DialogueSystem : MonoBehaviour
@@ -9,28 +10,28 @@ public class DialogueSystem : MonoBehaviour
     [Header("Personalize Text")]
     [SerializeField] private string input;
     [SerializeField] private Color textColor;
-    [SerializeField] private Font textFont;
+    [SerializeField] private TMP_FontAsset textFont;
     [SerializeField] private float delay;
     [SerializeField] private int fontSize;
     [SerializeField] private string nameFXsound;
-    private Text textHolder;
+    private TextMeshProUGUI textHolder;
 
     public void Start()
     {
-        textHolder = GetComponent<Text>();
+        textHolder = GetComponentInChildren<TextMeshProUGUI>();
         StartCoroutine(WriteText(input, textHolder, textColor, textFont, delay, fontSize, nameFXsound));       
     }
 
-    public IEnumerator WriteText(string input, Text textHolder, Color textColor, Font textFont, float delay, int textSize, string nameFXsound)
+    public IEnumerator WriteText(string input, TextMeshProUGUI textHolder, Color textColor, TMP_FontAsset textFont, float delay, int textSize, string nameFXsound)
     {
         textHolder.font = textFont;
-        textHolder.color = textColor;
+        //textHolder.color = textColor;
         textHolder.fontSize = textSize;
         for (int i = 0; i < input.Length; i++)
         {
             textHolder.text += input[i];
             SoundManager.instance.Play(nameFXsound);
-            yield return new WaitForSeconds(delay / Random.Range(0.2f, 0.5f) );
+            yield return new WaitForSeconds(delay / Random.Range(0.2f, 1f) );
         }
     }
 }
