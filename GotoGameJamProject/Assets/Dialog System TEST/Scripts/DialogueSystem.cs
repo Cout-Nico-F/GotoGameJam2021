fe42296b278a5f32a05380808c81c7df5604d2e2
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2021-11-02 14:50:24 -0300

    Agregados a DialogueSystem
    Agregado multiples lineas de texto en DialogueSystem

--- GotoGameJamProject/Assets/Dialog System TEST/Scripts/DialogueSystem.cs
+++ GotoGameJamProject/Assets/Dialog System TEST/Scripts/DialogueSystem.cs
@@ -5,33 +5,46 @@ using UnityEngine;
 using TMPro;
 using AudioJam;
 
-public class DialogueSystem : MonoBehaviour
+namespace DialogueJam
 {
-    [Header("Personalize Text")]
-    [SerializeField] private string input;
-    [SerializeField] private Color textColor;
-    [SerializeField] private TMP_FontAsset textFont;
-    [SerializeField] private float delay;
-    [SerializeField] private int fontSize;
-    [SerializeField] private string nameFXsound;
-    private TextMeshProUGUI textHolder;
-
-    public void Start()
+    public class DialogueSystem : MonoBehaviour
     {
-        textHolder = GetComponentInChildren<TextMeshProUGUI>();
-        StartCoroutine(WriteText(input, textHolder, textColor, textFont, delay, fontSize, nameFXsound));       
-    }
+        [Header("Personalize Text")]
+        [SerializeField] private Color textColor;
+        [SerializeField] private TMP_FontAsset textFont;
+        [SerializeField] private float delay;
+        [SerializeField] private int fontSize;
+        [SerializeField] private string nameFXsound;
+        [Header("Dialogues")]
+        [SerializeField] private float delayBetweenDialogues;
+        [SerializeField] private string[] textInput;
+        private TextMeshProUGUI textHolder;
 
-    public IEnumerator WriteText(string input, TextMeshProUGUI textHolder, Color textColor, TMP_FontAsset textFont, float delay, int textSize, string nameFXsound)
-    {
-        textHolder.font = textFont;
-        //textHolder.color = textColor;
-        textHolder.fontSize = textSize;
-        for (int i = 0; i < input.Length; i++)
+        public void Start()
         {
-            textHolder.text += input[i];
-            SoundManager.instance.Play(nameFXsound);
-            yield return new WaitForSeconds(delay / Random.Range(0.2f, 1f) );
+            textHolder = GetComponentInChildren<TextMeshProUGUI>();
+            StartCoroutine(RecibeText(textInput));
+        }
+        public IEnumerator RecibeText(string[] textInput)
+        {
+            for (int i = 0; i < textInput.Length; i++)
+            {
+                StartCoroutine(WriteText(textInput[i], textHolder, textColor, textFont, delay, fontSize, nameFXsound));
+                yield return new WaitForSeconds(delayBetweenDialogues);
+            }
+        }
+        public IEnumerator WriteText(string input, TextMeshProUGUI textHolder, Color textColor, TMP_FontAsset textFont, float delay, int textSize, string nameFXsound)
+        {
+            textHolder.text = "";
+            textHolder.font = textFont;
+            textHolder.color = textColor;
+            textHolder.fontSize = textSize;
+            for (int i = 0; i < input.Length; i++)
+            {
+                textHolder.text += input[i];
+                SoundManager.instance.Play(nameFXsound);
+                yield return new WaitForSeconds(delay / Random.Range(0.2f, 1f));
+            }
         }
     }
 }
