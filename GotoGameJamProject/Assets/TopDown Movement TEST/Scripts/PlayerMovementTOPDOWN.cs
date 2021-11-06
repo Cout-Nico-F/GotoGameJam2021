f1b7d7d3e377ed996b95688a4cef52874fddb8b3
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2021-11-06 18:58:21 -0300

    Animaciones TOP-DOWN

--- GotoGameJamProject/Assets/TopDown Movement TEST/Scripts/PlayerMovementTOPDOWN.cs
+++ GotoGameJamProject/Assets/TopDown Movement TEST/Scripts/PlayerMovementTOPDOWN.cs
@@ -4,29 +4,39 @@ using UnityEngine;
 
 public class PlayerMovementTOPDOWN : MonoBehaviour
 {
-    Rigidbody2D rb2d;
-    Animator animator;
+    private Rigidbody2D rb2d;
+    private Animator animator;
     [SerializeField] private float movementSpeed;
-    Vector2 movement;
-    // Start is called before the first frame update
+    private Vector2 movement;
+    private SpriteRenderer spriteRenderer;
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        if (movement != new Vector2(0, 0))
-        { animator.SetBool("run", true); }
-        else
-        { animator.SetBool("run", false); }
+        ControlAnimaciones();
+
     }
     private void FixedUpdate()
     {
         rb2d.MovePosition(rb2d.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+    private void ControlAnimaciones()
+    {
+        if (movement != new Vector2(0, 0))
+        { animator.SetBool("run", true); }
+        else
+        { animator.SetBool("run", false); }
+        if (movement.x < 0)
+        { spriteRenderer.flipX = true; }
+        if (movement.x > 0)
+        { spriteRenderer.flipX = false; }
     }
 }
