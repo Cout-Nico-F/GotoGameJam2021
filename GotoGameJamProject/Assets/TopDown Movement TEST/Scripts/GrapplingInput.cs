bbdd3e14398acd494c2178f8405ebb22f40cce9e
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2021-11-07 20:19:44 -0300

    Player prefabs y cambios en Grappling

--- GotoGameJamProject/Assets/TopDown Movement TEST/Scripts/GrapplingInput.cs
+++ GotoGameJamProject/Assets/TopDown Movement TEST/Scripts/GrapplingInput.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 namespace GrapplingJam
@@ -7,56 +8,61 @@ namespace GrapplingJam
     public class GrapplingInput : MonoBehaviour
     {
         [SerializeField] private GrapplingGun GGS;
+        [SerializeField] private PhotonView photonView;
         [SerializeField] private float maxGrapplingTimer;
         private float grapplingTimer;
         private bool canGrappling;
 
+
         private void Update()
         {
-            if(!canGrappling)
+            if (photonView.IsMine)
             {
-                if(grapplingTimer<maxGrapplingTimer)
-                { grapplingTimer += Time.deltaTime; }
-                else
-                { grapplingTimer = 0;canGrappling = true; }
-            }
-
-            if (Input.GetKeyDown(KeyCode.Mouse0)&&canGrappling)
-            {
-                GGS.SetGrapplePoint();canGrappling = false;
-            }
-            else if (Input.GetKey(KeyCode.Mouse0))
-            {
-                if (GGS.grappleRope.enabled)
+                if (!canGrappling)
                 {
-                    GGS.RotateGun(GGS.GrapplePoint, false);
+                    if (grapplingTimer < maxGrapplingTimer)
+                    { grapplingTimer += Time.deltaTime; }
+                    else
+                    { grapplingTimer = 0; canGrappling = true; }
+                }
+
+                if (Input.GetKeyDown(KeyCode.Mouse0) && canGrappling)
+                {
+                    GGS.SetGrapplePoint(); canGrappling = false;
+                }
+                else if (Input.GetKey(KeyCode.Mouse0))
+                {
+                    if (GGS.grappleRope.enabled)
+                    {
+                        GGS.RotateGun(GGS.GrapplePoint, false);
+                    }
+                    else
+                    {
+                        Vector2 mousePos = GGS.Camera.ScreenToWorldPoint(Input.mousePosition);
+                        GGS.RotateGun(mousePos, true);
+                    }
+
+                    if (GGS.LaunchToPoint && GGS.grappleRope.isGrappling)
+                    {
+                        if (GGS.LaunchType == LaunchType.Transform_Launch)
+                        {
+                            Vector2 firePointDistnace = GGS.FirePoint.position - GGS.GunHolder.localPosition;
+                            Vector2 targetPos = GGS.GrapplePoint - firePointDistnace;
+                            GGS.GunHolder.position = Vector2.Lerp(GGS.GunHolder.position, targetPos, Time.deltaTime * GGS.LaunchSpeed);
+                        }
+                    }
+                }
+                else if (Input.GetKeyUp(KeyCode.Mouse0))
+                {
+                    GGS.grappleRope.enabled = false;
+                    GGS.SpringJoint2D.enabled = false;
+                    GGS.Rigidbody.gravityScale = 0;
                 }
                 else
                 {
                     Vector2 mousePos = GGS.Camera.ScreenToWorldPoint(Input.mousePosition);
                     GGS.RotateGun(mousePos, true);
                 }
-
-                if (GGS.LaunchToPoint && GGS.grappleRope.isGrappling)
-                {
-                    if (GGS.LaunchType == LaunchType.Transform_Launch)
-                    {
-                        Vector2 firePointDistnace = GGS.FirePoint.position - GGS.GunHolder.localPosition;
-                        Vector2 targetPos = GGS.GrapplePoint - firePointDistnace;
-                        GGS.GunHolder.position = Vector2.Lerp(GGS.GunHolder.position, targetPos, Time.deltaTime * GGS.LaunchSpeed);
-                    }
-                }
-            }
-            else if (Input.GetKeyUp(KeyCode.Mouse0))
-            {
-                GGS.grappleRope.enabled = false;
-                GGS.SpringJoint2D.enabled = false;
-                GGS.Rigidbody.gravityScale = 0;
-            }
-            else
-            {
-                Vector2 mousePos = GGS.Camera.ScreenToWorldPoint(Input.mousePosition);
-                GGS.RotateGun(mousePos, true);
             }
         }
     }
--- GotoGameJamProject/Assets/TopDown Movement TEST/Scripts/GrapplingRope.cs
+++ GotoGameJamProject/Assets/TopDown Movement TEST/Scripts/GrapplingRope.cs
@@ -4,20 +4,20 @@ using GrapplingJam;
 public class GrapplingRope : MonoBehaviour
 {
     [Header("General Refernces:")]
-    public GrapplingGun grapplingGun;
-    public LineRenderer m_lineRenderer;
+    [SerializeField] private GrapplingGun grapplingGun;
+    [SerializeField] private LineRenderer m_lineRenderer;
 
     [Header("General Settings:")]
     [SerializeField] private int percision = 40;
     [Range(0, 20)] [SerializeField] private float straightenLineSpeed = 5;
 
     [Header("Rope Animation Settings:")]
-    public AnimationCurve ropeAnimationCurve;
+    [SerializeField] private AnimationCurve ropeAnimationCurve;
     [Range(0.01f, 4)] [SerializeField] private float StartWaveSize = 2;
     float waveSize = 0;
 
     [Header("Rope Progression:")]
-    public AnimationCurve ropeProgressionCurve;
+    [SerializeField] private AnimationCurve ropeProgressionCurve;
     [SerializeField] [Range(1, 50)] private float ropeProgressionSpeed = 1;
 
     float moveTime = 0;
@@ -32,6 +32,7 @@ public class GrapplingRope : MonoBehaviour
         m_lineRenderer.positionCount = percision;
         waveSize = StartWaveSize;
         strightLine = false;
+        grapplingGun = GetComponentInParent<GrapplingGun>();
 
         LinePointsToFirePoint();
 
