90318ca7b40bd8a4db2e81d3852f74c81c33997c
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2021-11-08 23:21:51 -0300

    Funca el puntaje

--- GotoGameJamProject/Assets/Multiplayer Photon TEST/LevelUI.cs
+++ GotoGameJamProject/Assets/Multiplayer Photon TEST/LevelUI.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
 using TMPro;
 
 public class LevelUI : MonoBehaviour
@@ -7,30 +8,36 @@ public class LevelUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private Button addScoreButton;
     [SerializeField] private Score score;
+    [SerializeField] private PhotonView playerPhotonView;
+    [SerializeField] private GameObject desactivateCanvas;
 
     public Score Score { get => score; set => score = value; }
 
     private void Awake()
     {
-        score.PlayerScore = 0;
-        addScoreButton.onClick.AddListener(AddScore);
+        if(!playerPhotonView.IsMine)
+        {
+            desactivateCanvas.SetActive(false);
+        }
+            score.PlayerScore = 0;
+            addScoreButton.onClick.AddListener(AddScore);
     }
 
     private void Start()
     {
-        scoreText.text = score.PlayerScore + "/" ;
+            scoreText.text = score.PlayerScore + "/";
     }
 
     public void AddScore()
     {
-        score.PlayerScore++;
-        scoreText.text = score.PlayerScore + "/";
+            score.PlayerScore++;
+            scoreText.text = score.PlayerScore + "/";
     }
 
     public void ResetScore()
     {
-        score.PlayerScore = 0;
-        scoreText.text = score.PlayerScore + "/" ;
+            score.PlayerScore = 0;
+            scoreText.text = score.PlayerScore + "/";
     }
 }
 
