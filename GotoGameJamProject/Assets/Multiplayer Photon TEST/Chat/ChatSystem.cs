fb05f9910401a810ce812f19bc25917f3f3530f8
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2021-11-11 01:33:44 -0300

    Al hablar habla el otro, lo arreglo mañana  xd

--- GotoGameJamProject/Assets/Multiplayer Photon TEST/Chat/ChatSystem.cs
+++ GotoGameJamProject/Assets/Multiplayer Photon TEST/Chat/ChatSystem.cs
@@ -1,6 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+using Photon.Pun;
 using UnityEngine;
 
 
@@ -8,9 +6,18 @@ public class ChatSystem : MonoBehaviour
 {
     [Header("Enter parameters")]
     [SerializeField] private TextFramePlayer textFramePayer;
+    [SerializeField] private PhotonView photonView;
+    private void Start()
+    {
+        if(!photonView.IsMine)
+        { gameObject.SetActive(false);}
+    }
     public void ReadInptPlayer(string s)
     {
-        textFramePayer.gameObject.SetActive(true);
-        textFramePayer.StartCoroutine(textFramePayer.PlayerTalk(s));
+        if (photonView.IsMine)
+        {
+            textFramePayer.gameObject.SetActive(true);
+            textFramePayer.StartCoroutine(textFramePayer.PlayerTalk(s));
+        }
     }
 }
--- GotoGameJamProject/Assets/Multiplayer Photon TEST/Chat/TextFramePlayer.cs
+++ GotoGameJamProject/Assets/Multiplayer Photon TEST/Chat/TextFramePlayer.cs
@@ -1,25 +1,35 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
 using TMPro;
 using UnityEngine;
+using Photon.Pun;
 
-    public class TextFramePlayer : MonoBehaviour
+public class TextFramePlayer : MonoBehaviour
+{
+    [Header("Enter parameters")]
+    [SerializeField] private TextMeshProUGUI textMesh;
+    [SerializeField] private PhotonView photonView;
+    [Header("Text settings")]
+    [SerializeField] private Color textColor;
+    private void Start()
     {
-        [Header("Enter parameters")]
-        [SerializeField] private TextMeshProUGUI textMesh;
-        [Header("Text settings")]
-        [SerializeField] private Color textColor;
-
-        private void Start()
-        {
-            textMesh.color = textColor;
-        }
-        public IEnumerator PlayerTalk(string textInput)
-        {
-            textMesh.text=textInput;
-
-            yield return new WaitForSeconds(3);
-            gameObject.SetActive(false);
-        }
+        textMesh.color = textColor;
     }
+
+
+    public IEnumerator PlayerTalk(string textInput)
+    {
+            textMesh.text = "";
+            textMesh.text = textInput;
+            photonView.RPC("SyncText", RpcTarget.All, textInput);
+            yield return new WaitForSeconds(3);
+            photonView.RPC("SyncText", RpcTarget.All, "");
+            gameObject.SetActive(false);
+    }
+
+    [PunRPC]
+    void SyncText(string textInput)
+    {
+            textMesh.text = "";
+            textMesh.text = textInput;
+    }
+}
