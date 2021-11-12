a5eb75d55411e1afbc416db2941b27d593cfbbb6
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2021-11-12 17:36:19 -0300

    El chat funciona :3 :100:

--- GotoGameJamProject/Assets/Multiplayer Photon TEST/Chat/ChatSystsem.cs
+++ GotoGameJamProject/Assets/Multiplayer Photon TEST/Chat/ChatSystsem.cs
@@ -1,12 +1,20 @@
 using TMPro;
 using UnityEngine;
+using Photon.Pun;
 
 public class ChatSystsem : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textMesh;
+    [SerializeField] private PhotonView photonView;
 
-    public void PlayerTalk(string textInput)
+    public void PlayerTalk(string textPlayerInput)
     {
-        textMesh.text = textMesh.text + "\n" + textInput;
+        photonView.RPC("SyncTextPlayerChat", RpcTarget.All, textPlayerInput); 
+    }
+
+    [PunRPC]
+    public void SyncTextPlayerChat(string textPlayerInput)
+    {
+        textMesh.text = textMesh.text + "\n" + textPlayerInput;
     }
 }
