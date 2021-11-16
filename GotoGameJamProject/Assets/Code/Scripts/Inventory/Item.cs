f48268c388b87568447d670eb7bb11eb9eb32fbf
jhcanti <jhcanti@gmail.com>
2021-11-16 17:37:17 +0100

    Añado Id a la clase Item

--- GotoGameJamProject/Assets/Code/Scripts/Inventory/Item.cs
+++ GotoGameJamProject/Assets/Code/Scripts/Inventory/Item.cs
@@ -5,7 +5,10 @@ using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
+    [SerializeField] private string id;
     [SerializeField] private Sprite sprite;
+
     public Sprite Sprite { get => sprite; }
+    public string Id { get => id; }
 
 }
