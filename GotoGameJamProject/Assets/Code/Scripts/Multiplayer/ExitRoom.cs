11cf4235350709d96cdbc4b9deb37f3cf436d4c7
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2021-11-18 00:23:32 -0300

    Salir de la partida

---
+++ GotoGameJamProject/Assets/Code/Scripts/Multiplayer/ExitRoom.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Photon.Pun;
+
+public class ExitRoom : MonoBehaviour
+{
+    public void exitRoom()
+    {
+        PhotonNetwork.DestroyPlayerObjects(PhotonNetwork.LocalPlayer);
+        PhotonNetwork.LeaveRoom();
+        PhotonNetwork.LoadLevel(0);
+    }
+}
