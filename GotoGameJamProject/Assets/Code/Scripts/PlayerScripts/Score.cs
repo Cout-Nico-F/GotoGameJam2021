e7f255c9dd9c1b1ee99c73fee74db35d40937b0c
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2021-11-18 22:24:47 -0300

    Score se sincroniza

--- GotoGameJamProject/Assets/Code/Scripts/PlayerScripts/Score.cs
+++ GotoGameJamProject/Assets/Code/Scripts/PlayerScripts/Score.cs
@@ -1,7 +1,19 @@
 using UnityEngine;
-
-public class Score : MonoBehaviour
+using Photon.Pun;
+public class Score : MonoBehaviour,IPunObservable
 {
-    [SerializeField] int playerScore;
+    [HideInInspector] public int playerScore;
     public int PlayerScore { get => playerScore; set => playerScore = value; }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.IsWriting)
+        {
+            stream.SendNext(playerScore);
+        }
+        else
+        {
+            playerScore = (int)stream.ReceiveNext();
+        }
+    }
 }
