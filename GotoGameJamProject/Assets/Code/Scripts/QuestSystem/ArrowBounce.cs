1eec3232a4456d31051e4e23856f6b4cbf478ea0
jhcanti <jhcanti@gmail.com>
2021-11-20 17:07:26 +0100

    Ahora aparece el item requerido en la quest con una flecha rebotando

--- GotoGameJamProject/Assets/Code/Scripts/QuestSystem/ArrowBounce.cs
+++ GotoGameJamProject/Assets/Code/Scripts/QuestSystem/ArrowBounce.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class ArrowBounce : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float amount;
+    [SerializeField] private float velocity;
+    private Vector2 destination;
+    private Vector2 originPosition;
+    private float t;
+
+    private void OnEnable()
     {
-        
+        originPosition = transform.localPosition;
+        destination = originPosition + new Vector2(0, amount);
+        t = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        transform.localPosition = new Vector3(0, Mathf.Lerp(originPosition.y, destination.y, t), 0);
+        t += velocity * Time.deltaTime;
+        if (t > 1f)
+        {
+            var temp = destination;
+            destination = originPosition;
+            originPosition = temp;
+            t = 0;
+        }
     }
 }
