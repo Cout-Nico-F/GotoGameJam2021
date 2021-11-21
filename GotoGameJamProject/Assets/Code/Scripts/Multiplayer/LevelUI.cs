4fa9d2d0daef30e8edf841b282e08cd750eadc01
CNicoF <nicolasferreira-externo@tgacompany.com>
2021-11-21 20:13:37 -0300

    Update LevelUI.cs

--- GotoGameJamProject/Assets/Code/Scripts/Multiplayer/LevelUI.cs
+++ GotoGameJamProject/Assets/Code/Scripts/Multiplayer/LevelUI.cs
@@ -5,7 +5,6 @@ using TMPro;
 
 public class LevelUI : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private PhotonView playerPhotonView;
     [SerializeField] private GameObject desactivateCanvas;
 
@@ -14,13 +13,7 @@ public class LevelUI : MonoBehaviour
         if (!playerPhotonView.IsMine)
         {
             desactivateCanvas.SetActive(false);
-        }
-        
+        }      
     }
-    private void Update()
-    {
-        scoreText.text = RoomController.Instance.GoalActual.ToString();
-    }
-
 }
 
