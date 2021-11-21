35bfb8a97ad27b2c985e4df379d7e58f86ceaffe
CNicoF <nicolasferreira-externo@tgacompany.com>
2021-11-20 23:58:13 -0300

    Splash screen lista

--- GotoGameJamProject/Assets/Code/Scripts/SplashScreen/JamAnimation.cs
+++ GotoGameJamProject/Assets/Code/Scripts/SplashScreen/JamAnimation.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class JamAnimation : MonoBehaviour
 {
-    
     public void PlayWoosh1()
     {
         AudioJam.SoundManager.instance.Play("Woosh1");
     }
-
     public void PlayWoosh2()
     {
         AudioJam.SoundManager.instance.Play("Woosh2");
+    }
+    public void PlayGotouu()
+    {
+        AudioJam.SoundManager.instance.Play("Gotouu");
+    }
+    public void PlayPimm()
+    {
+        AudioJam.SoundManager.instance.Play("Pimm");
     }
 
 }
