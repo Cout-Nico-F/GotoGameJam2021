a1f2ea23604963e20c2bc2291676e2044b2f2f12
CNicoF <nicolasferreira-externo@tgacompany.com>
2021-11-21 01:45:01 -0300

    Spiral sprite + CineMusic -SpawnTravelEffect + SoundManager override +etc

--- GotoGameJamProject/Assets/Code/Scripts/Interactuable/AnimationInitialCinematic.cs
+++ GotoGameJamProject/Assets/Code/Scripts/Interactuable/AnimationInitialCinematic.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnTravelEffect : MonoBehaviour
+public class AnimationInitialCinematic : MonoBehaviour
 {
     [SerializeField] private GameObject effect;
     public void TravelEffect()
     {
         effect.SetActive(true);
+    }
+
+    public void PlayCineMusic()
+    {
+        AudioJam.SoundManager.instance.Play("CineMusic");
     }
 }
