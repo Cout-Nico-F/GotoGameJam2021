a59086a8312413ce0853a5dc333461e0741709eb
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2021-11-21 23:24:45 -0300

    Room controller arreglado

--- GotoGameJamProject/Assets/Code/Scripts/Multiplayer/RoomController.cs
+++ GotoGameJamProject/Assets/Code/Scripts/Multiplayer/RoomController.cs
@@ -6,23 +6,19 @@ using Photon.Pun;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class RoomController : MonoBehaviour,IPunObservable
+public class RoomController : MonoBehaviour, IPunObservable
 {
     private static RoomController instance;
-    public static RoomController Instance => instance; 
-    [SerializeField] private float minutes;
-    [SerializeField] private float seconds;
+    public static RoomController Instance => instance;
     [SerializeField] private float minutesGracia;
     [SerializeField] private float secondsGracia;
-    [SerializeField] private int goalActual; 
+    [SerializeField] private int goalActual;
     [SerializeField] private int goal;
     [SerializeField] private TextMeshProUGUI textMeshPro;
     [SerializeField] private Image image;
     bool firstTime = true;
     private LevelUI levelUI;
 
-    public float Minutes { get => minutes; set => minutes = value; }
-    public float Seconds { get => seconds; set => seconds = value; }
     public float MinutesGracia { get => minutesGracia; set => minutesGracia = value; }
     public float SecondsGracia { get => secondsGracia; set => secondsGracia = value; }
     public int GoalActual { get => goalActual; set => goalActual = value; }
@@ -42,17 +38,13 @@ public class RoomController : MonoBehaviour,IPunObservable
         goalActual = 0;
         firstTime = true;
     }
-    void Start()
-    {
-        textMeshPro.enabled = false;
-    }
     private void Update()
     {
-        if(goal != 0)
+        if (goal != 0)
         {
             image.fillAmount = (float)goalActual / (float)goal;
         }
-
+        textMeshPro.text = minutesGracia + ":" + Mathf.Round(secondsGracia);
         if (minutesGracia >= 0 && secondsGracia > 0)
         {
             secondsGracia -= Time.deltaTime;
@@ -64,35 +56,16 @@ public class RoomController : MonoBehaviour,IPunObservable
         }
         else
         {
-            if(firstTime)
-            {
-                goal = 2 + PhotonNetwork.CurrentRoom.PlayerCount * 3;
-                firstTime = false;
-                textMeshPro.enabled = true;
-            }
+            if (goalActual >= goal)
+            {///aca ganaron
+                StartCoroutine(Disconnect("WinGame"));
 
-            textMeshPro.text = minutes + ":" + Mathf.Round(seconds);
-            if (minutes >= 0 && seconds > 0)
-            {
-                seconds -= Time.deltaTime;
-                if (seconds <= 0)
-                {
-                    minutes--;
-                    seconds = 60;
-                }
             }
             else
-            {
-                if(goalActual >= goal)
-                {///aca ganaron
-                    StartCoroutine(Disconnect("WinGame"));
-                    
-                }
-                else
-                {///aca perdieron{
-                    StartCoroutine(Disconnect("LooseGame"));
-                } 
+            {///aca perdieron{
+                StartCoroutine(Disconnect("LooseGame"));
             }
+
         }
     }
     IEnumerator Disconnect(string input)
@@ -109,10 +82,8 @@ public class RoomController : MonoBehaviour,IPunObservable
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        if(stream.IsWriting)
+        if (stream.IsWriting)
         {
-            stream.SendNext(minutes);
-            stream.SendNext(seconds);
             stream.SendNext(minutesGracia);
             stream.SendNext(secondsGracia);
             stream.SendNext(goal);
@@ -120,8 +91,6 @@ public class RoomController : MonoBehaviour,IPunObservable
         }
         else
         {
-            minutes = (float)stream.ReceiveNext();
-            seconds = (float)stream.ReceiveNext();
             minutesGracia = (float)stream.ReceiveNext();
             secondsGracia = (float)stream.ReceiveNext();
             goal = (int)stream.ReceiveNext();
