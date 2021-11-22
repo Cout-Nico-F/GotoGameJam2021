f5a42a96e0b7a888ebb075fe2854a94c1b507f67
CNicoF <nicolasferreira-externo@tgacompany.com>
2021-11-22 00:39:56 -0300

    Musica arreglada

--- GotoGameJamProject/Assets/Code/Scripts/Sound/SoundManager.cs
+++ GotoGameJamProject/Assets/Code/Scripts/Sound/SoundManager.cs
@@ -45,7 +45,7 @@ namespace AudioJam
             }
 
             s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
-            s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+            s.source.pitch = s.pitch * (1f);
             s.source.Play();
         }
 
@@ -59,7 +59,7 @@ namespace AudioJam
             }
 
             s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
-            s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+            s.source.pitch = s.pitch * (1f);
 
             s.source.Stop();
         }
