460cb0da3451a61d28a4ed8ba4f3690b44d04bf8
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2021-11-26 16:20:44 -0300

    Radio ahora es local

--- GotoGameJamProject/Assets/Code/Scripts/Interactuable/Radio.cs
+++ GotoGameJamProject/Assets/Code/Scripts/Interactuable/Radio.cs
@@ -1,17 +1,9 @@
 using UnityEngine;
-using Photon.Pun;
 
 public class Radio : MonoBehaviour, Interactable
 {
     public AudioSource audioSource;
-    public PhotonView photonView;
-    private bool isPlaying;
     public void Interact(GameObject gameObject)
-    {
-        photonView.RPC("PlayAndStopMusic", RpcTarget.All);
-    }
-    [PunRPC]
-    public void PlayAndStopMusic()
     {
         if (audioSource.isPlaying)
         {
