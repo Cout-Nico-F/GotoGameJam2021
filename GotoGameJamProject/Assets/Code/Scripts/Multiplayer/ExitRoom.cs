e721526e76fbebae890ce6f29acf48085355a2e1
CNicoF <nicolasferreira-externo@tgacompany.com>
2021-11-27 16:48:26 -0300

    Cambiado el evento del boton desde ui a codigo listener

--- GotoGameJamProject/Assets/Code/Scripts/Multiplayer/ExitRoom.cs
+++ GotoGameJamProject/Assets/Code/Scripts/Multiplayer/ExitRoom.cs
@@ -3,8 +3,21 @@ using Photon.Pun;
 
 public class ExitRoom : MonoBehaviour
 {
-    public void exitRoom()
+    private void Awake()
     {
+        GetComponent<UnityEngine.UI.Button>().onClick.AddListener(OnExitRoom);
+    }
+
+    public void OnExitRoom()
+    {
+        //foreach (var item in AudioJam.SoundManager.instance.sounds)
+        //{
+        //    if (item.source.isPlaying)
+        //    {
+        //        AudioJam.SoundManager.instance.Stop(item.name);
+        //    }
+        //}
+        
         PhotonNetwork.DestroyPlayerObjects(PhotonNetwork.LocalPlayer);
         PhotonNetwork.LeaveRoom();
         PhotonNetwork.LoadLevel("Loading");
